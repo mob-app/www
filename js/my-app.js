3138db0c965a61ae8fc8f033a094fd3692d5dbf2
mob-app <sahana.hyne@mrdmedia.com>
2016-08-30 01:26:30 +0530

    Add files via upload

---
+++ js/my-app.js
@@ -0,0 +1,30 @@
+// Let's register Template7 helper so we can pass json string in links
+Template7.registerHelper('json_stringify', function (context) {
+    return JSON.stringify(context);
+});
+
+// Initialize your app
+ var myApp = new Framework7({
+    swipeBackPage: false,
+    pushState: true,
+    swipePanel: 'left',
+    modalTitle: 'Title',
+});
+ 
+// Export selectors engine
+var $$ = Dom7;
+
+// Add main View
+var mainView = myApp.addView('.view-main', {
+    // Enable dynamic Navbar
+    dynamicNavbar: true,
+});
+
+
+var mySwiper = myApp.swiper('.swiper-container', {
+  pagination: '.swiper-pagination',
+  paginationHide: false,
+  paginationClickable: true,
+  nextButton: '.swiper-button-next',
+  prevButton: '.swiper-button-prev',
+});     
